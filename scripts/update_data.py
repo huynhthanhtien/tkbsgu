ab52bbf6a680da494dbdb72cd4aa5a62d74e57a1
os <huynhthanhtien830@gmail.com>
2025-08-31 01:08:41 +0700

    update

---
+++ scripts/update_data.py
@@ -0,0 +1,17 @@
+import requests
+import json
+from datetime import datetime
+import os 
+
+
+# def fetch_data(username, password):
+
+
+
+def main():
+    password = os.getenv("PASSWORD")
+    username = os.getenv("USERNAME")    
+
+
+if __name__ == "__main__":
+    print("main")
